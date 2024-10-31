2d3e7edcdd612b104a8cb8ba668b897d7dc1c99b
Esfot <esfot26@gmail.com>
2024-10-31 19:15:41 -0300

    mas cambios de vistas

--- client/src/assets/components/vendedores/VendedorEditar.jsx
+++ client/src/assets/components/vendedores/VendedorEditar.jsx
@@ -76,6 +76,8 @@ const EditarVendedor = () => {
         <>
             <Helmet>
                 <meta charSet="UTF-8" />
+                <link rel="icon" type="image/svg+xml" href="/src/assets/images/editar.png" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <title>Editar Vendedor</title>
             </Helmet>
             <Container
